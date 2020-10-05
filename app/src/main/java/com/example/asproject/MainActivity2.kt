6d91d766343c18541f89bd94797c4ad427f9aef8
MykolaMeln <Kolamelnychuk2000@gmail.com>
2020-10-05 19:29:23 +0300

    Update lab2

--- app/src/main/java/com/example/asproject/MainActivity2.kt
+++ app/src/main/java/com/example/asproject/MainActivity2.kt
@@ -1,10 +1,18 @@
 package com.example.asproject
 
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.RadioButton
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_main2.*
+import kotlinx.android.synthetic.main.activity_main3.*
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +28,16 @@ class MainActivity2 : AppCompatActivity() {
     fun toActivity3(view: View?) {
         val intent = Intent(this@MainActivity2, MainActivity3::class.java)
         startActivity(intent)
+    }
+
+    fun exit(view: View?)
+    {
+        finish()
+    }
+
+    fun theme(view: View?)
+    {
+        gl.setBackgroundColor(Color.parseColor("#636363"))
     }
 
 }
